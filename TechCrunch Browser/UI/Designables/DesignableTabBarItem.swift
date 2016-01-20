b3690b49c4526c3fa28b5c4397c576a2e0da7d9c
JamStop <jimmyyue1@gmail.com>
2016-01-20 15:17:19 -0800

    feed layout

---
+++ TechCrunch Browser/UI/Designables/DesignableTabBarItem.swift
@@ -0,0 +1,21 @@
+//
+//  DesignableTabBarItem.swift
+//  TechCrunch Browser
+//
+//  Created by Jimmy Yue on 1/20/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+//import UIKit
+//
+//@IBDesignable public class DesignableTabBarItem: UITabBarItem {
+//    
+//    @IBInspectable public var borderColor: UIColor = UIColor.clearColor() {
+//        didSet {
+//            layer.borderColor = borderColor.CGColor
+//        }
+//    }
+//    
+//    @IBInspectable public var 
+//    
+//}
