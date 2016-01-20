3ba71ae34ca8d37f0d94c34c3839615b6a2bb18c
JamStop <jimmyyue1@gmail.com>
2016-01-20 10:56:35 -0800

    init

---
+++ TechCrunch Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechCrunch Browser
+//
+//  Created by Jimmy Yue on 1/20/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
