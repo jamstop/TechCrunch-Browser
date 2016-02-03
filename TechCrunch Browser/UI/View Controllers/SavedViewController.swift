7979747792566e7071f163e8f226780d377fcd5f
JamStop <jimmyyue1@gmail.com>
2016-02-02 17:47:25 -0800

    vc setup

---
+++ TechCrunch Browser/UI/View Controllers/SavedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SavedViewController.swift
+//  TechCrunch Browser
+//
+//  Created by Jimmy Yue on 2/2/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+import UIKit
+
+class SavedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ TechCrunch Browser/UI/Views/Saved/SavedView.swift
@@ -0,0 +1,9 @@
+//
+//  SavedView.swift
+//  TechCrunch Browser
+//
+//  Created by Jimmy Yue on 2/2/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+import Foundation
